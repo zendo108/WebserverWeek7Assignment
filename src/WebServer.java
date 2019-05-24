b332a759807b60467706c945fdce9a0327024a77
Ivan Aranda <IvanAranda@iMac.local>
2019-05-24 14:31:55 -0400

    added pages and resources, image, pdf, and .html files, and added threads, this is a working project

--- src/WebServer.java
+++ src/WebServer.java
@@ -24,11 +24,17 @@ Port Common Name RFC#  Purpose
 
 ///A Simple Web Server (WebServer.java)
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 /**
  * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
@@ -40,170 +46,139 @@ import java.net.Socket;
  * @author Jeff Heaton
  * @version 1.0
  */
-/*
- * Examining the Mini Web Server
+
+/**
  * 
- * Server sockets use the ServerSocket object rather than the Socket object that
- * client sockets use. There are several constructors available with the
- * ServerSocket object. The simplest constructor accepts only the port number on
- * which the program should be listening. Listening refers to the mode that a
- * server is in while it waits for clients to connect. The following lines of
- * code are used in Listing 1.3 to create a new ServerSocket object and reserve
- * port 80 as the port number on which the web server should listen for
- * connections:
+ * @author IvanAranda
+ * I studied and modify this Server implementation enough to serve .html .pdf .jpeg .jpg .png files
+ * and to support Thread 
+ *
+ * To test the server I put together for this assignment
+ * run this file
  * 
- * try { 
- * // create the main server 
- * socket s = new ServerSocket(80); 
- * }
- * catch(Exception e) { 
- * System.out.println("Error: " + e ); 
- * return; 
- * }
+ * the following will get you to index.html
+ * http://localhost:8080/
  * 
- * The try block is necessary because any number of errors could occur when the
- * program attempts to register port 80. The most common error that would result
- * is that there is already a server listening to port 80 on this machine.
- * Warning
+ * the following will get you to the image
+ * http://localhost:8080/nyc_image.jpg
  * 
- * This program will not work on a machine that already has a web server, or
- * some other program, listening on port 80.
+ * the following will get you to the PDF file
+ * http://localhost:8080/section6_5.pdf
  * 
- * Once the program has port 80 registered, it can begin listening for
- * connections. The following line of code is used to wait for a connection:
+ * the following will get you to the resorce not found 404.html page
+ * this goes for anything that you request and it is not in the server files
+ * http://localhost:8080/tests
  * 
- * Socket remote = s.accept();
- * 
- * The Socket object that is returned by accept is exactly the same class that
- * is used for client sockets. Once the connection is established, the
- * difference between client and server sockets fade. The primary difference
- * between client and server sockets is the way in which they connect. A client
- * sever connects to something. A server socket waits for something to connect
- * to it.
- * 
- * The accept method is a blocking call, which means the current thread will
- * wait for a connection. This can present problems for your program if there
- * are other tasks it would like to accomplish while it is waiting for
- * connections. Because of this, it is very common to see the accept method call
- * placed in a worker thread. This allows the main thread to carry on other
- * tasks, while the worker thread waits for connections to arrive.
- * 
- * Once a connection is made, the accept method will return a socket object for
- * the new socket. After this point, reading and writing is the same between
- * client and server sockets. Many client server programs would create a new
- * thread to handle this new connection.
- * 
- * Now that a connection has been made, a new thread could be created to handle
- * it. This new worker thread would process all the requests from this client in
- * the background, which allows the ServerSocket object to wait for and service
- * more connections. However, the example program in Listing 1.3 does not
- * require such programming. As soon as the socket is accepted, input and output
- * objects are created; this same process was used with the SMTP client. The
- * following lines from Listing 1.3 show the process of preparing the newly
- * accepted socket for input and output:
- * 
- * //remote is now the connected socket 
- * System.out.println("Connection, sending data."); 
- * BufferedReader in 
- * = new BufferedReader( new InputStreamReader(remote.getInputStream()) ); 
- * PrintWriter out = new PrintWriter(remote.getOutputStream());
- * 
- * Now that the program has input and output objects, it can process the HTTP
- * request. It first reads the HTTP request lines. A full-featured server would
- * parse each line and determine the exact nature of this request, however, our
- * ultra-simple web server just reads in the request lines and ignores them, as
- * shown here:
- * 
- * //read the data sent. We basically ignore it, 
- * //stop reading once a blank line is hit. This 
- * //blank line signals the end of the 
- * //client HTTP headers.
- *  String str="."; 
- * while(!str.equals("")) 
- * str = in.readLine();
- * 
- * These lines cause the server to read in lines of text from the newly
- * connected socket. Once a blank line (which indicates the end of the HTTP
- * header) is reached, the loop stops, and the server stops reading. Now that
- * the HTTP header has been retrieved, the server sends an HTTP response. The
- * following lines of code accomplish this:
- * 
- * //Send the response 
- * //Send the headers out.println("HTTP/1.0 200 OK");
- * out.println("Content-Type: text/html"); 
- * out.println("Server: Bot"); 
- * //this blank line signals the end of the headers out.println("");
- * // Send the HTML page out.println( " <H1> Welcome to the Ultra Mini-WebServer </H2> ");
- * 
- * Status code 200, as shown on line 3 of the preceding code, is used to show
- * that the page was properly transferred, and that the required HTTP headers
- * were sent. (Refer to Chapter 2 for more information about HTTP headers.)
- * Following the HTTP headers, the actual HTML page is transferred. Once the
- * page is transferred, the following lines of code from Listing 1.3 are
- * executed to clean up:
- * 
- * out.flush(); remote.close();
- * 
- * The flush method is necessary to ensure that all data is transferred, and the
- * close method is necessary to close the socket. Although Java will discard the
- * Socket object, it will not generally close the socket on most platforms.
- * Because of this, you must close the socket or else you might eventually get
- * an error indicating that there are no more file handles. This becomes very
- * important for a program that opens up many connections, including one to a
- * spider.
  */
-public class WebServer {
+public class WebServer  implements Runnable{
 
   /**
    * WebServer constructor.
    */
-  protected void start() {
-    ServerSocket s;
-
-    System.out.println("Webserver starting up on port 80");
-    System.out.println("(press ctrl-c to exit)");
-    try {
-      // create the main server socket
-      s = new ServerSocket(80);
-    } catch (Exception e) {
-      System.out.println("Error: " + e);
-      return;
-    }
+	static final String DEFAULT_FILE = "index.html";
+	static final String METHOD_NOT_IMPLEMENTED_FILE = "not_implemented.html";
+	static final File SERVER_ROOT = new File(".");
+	static final int SERVER_PORT = 8080;
+	
+	// verbose mode
+		static final boolean verbose = true;
+		
+	// Client Connection via Socket Class
+		private Socket acceptedSocket;
+		
+		public WebServer(Socket c) {
+			acceptedSocket = c;
+		}
+		
+@Override
+  public void run() {
 
     System.out.println("Waiting for connection");
-    for (;;) {
+
       try {
         // wait for a connection
-        Socket remote = s.accept();
+        Socket remote = acceptedSocket;
         // remote is now the connected socket
         System.out.println("Connection, sending data.");
         BufferedReader in = new BufferedReader(new InputStreamReader(
             remote.getInputStream()));
         PrintWriter out = new PrintWriter(remote.getOutputStream());
-
-        // read the data sent. We basically ignore it,
-        // stop reading once a blank line is hit. This
-        // blank line signals the end of the client HTTP
-        // headers.
-        String str = ".";
-        while (!str.equals(""))
-          str = in.readLine();
-
-        // Send the response
-        // Send the headers
-        out.println("HTTP/1.0 200 OK");
-        out.println("Content-Type: text/html");
-        out.println("Server: Bot");
-        // this blank line signals the end of the headers
-        out.println("");
-        // Send the HTML page
-        out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
-        out.flush();
+        BufferedOutputStream dataOut = new BufferedOutputStream(remote.getOutputStream());;
+        String input = in.readLine();
+        System.out.println("this is the input line "+input);
+        StringTokenizer parser = new StringTokenizer(input);
+        
+        String method = parser.nextToken().toUpperCase();
+        System.out.println("this is input token "+method);
+        
+        String fileRequested = parser.nextToken().toLowerCase();
+        System.out.println("this is the file requested "+fileRequested);
+        
+//        while (parser.hasMoreElements()) {
+//			System.out.println(parser.nextElement());
+//		}
+        if(method.equals("GET") || method.equals("HEAD")) {
+        	//if there is no file requested in the GET line
+        	if(fileRequested.equals("/")){
+        		//here  I append index.html 
+        		//because there are no file requested in the GET request
+        		fileRequested += DEFAULT_FILE;//<-- this is basically /index.html
+        	}
+        	//Now we create the response file
+        	//we pass it the root File and the file requested to complete the path
+        	//to the file requested
+        	File responseFile = new File(SERVER_ROOT,fileRequested);
+        	if(!responseFile.exists()) {
+        		fileRequested = "/404.html";
+        		responseFile = new File(SERVER_ROOT,fileRequested);
+        	}
+        	
+        	int fileLength = (int) responseFile.length();
+        	//the following method call make sure the file type, .html or else
+        	//if it ends with .html or .htm returns text/html else text/plain
+        	String content = getContentType(fileRequested);
+        	//if it is GET and not HEAD
+        	if(method.equals("GET")) {
+        		byte[] fileData = readFileData(responseFile, fileLength);
+				
+				// send HTTP Headers
+				out.println("HTTP/1.1 200 OK");
+				out.println("Server: Java HTTP Server from SSaurel : 1.0");
+				out.println("Date: " + new Date());
+				out.println("Content-type: " + content);
+				out.println("Content-length: " + fileLength);
+				out.println(); // blank line between headers and content, very important !
+				out.flush(); // flush character output stream buffer
+				
+				dataOut.write(fileData, 0, fileLength);
+				dataOut.flush();
+        	}
+        	
+        	
+        }else {
+        	// we return the not supported file to the client
+			File file = new File(SERVER_ROOT, METHOD_NOT_IMPLEMENTED_FILE);
+			int fileLength = (int) file.length();
+			String contentMimeType = "text/html";
+			//read content to return to client
+			byte[] fileData = readFileData(file, fileLength);
+				
+			// we send HTTP Headers with data to client
+			out.println("HTTP/1.1 501 Not Implemented");
+			out.println("Server: Java HTTP Server from SSaurel : 1.0");
+			out.println("Date: " + new Date());
+			out.println("Content-type: " + contentMimeType);
+			out.println("Content-length: " + fileLength);
+			out.println(); // blank line between headers and content, very important !
+			out.flush(); // flush character output stream buffer
+			// file
+			dataOut.write(fileData, 0, fileLength);
+			dataOut.flush();
+        }
         remote.close();
       } catch (Exception e) {
         System.out.println("Error: " + e);
       }
-    }
   }
 
   /**
@@ -213,7 +188,56 @@ public class WebServer {
    *            Command line parameters are not used.
    */
   public static void main(String args[]) {
-    WebServer ws = new WebServer();
-    ws.start();
+	  try {
+			ServerSocket serverConnect = new ServerSocket(SERVER_PORT);
+			System.out.println("Server started.\nListening for connections on port : " + SERVER_PORT + " ...\n");
+			
+			// we listen until user halts server execution
+			while (true) {
+				WebServer myServer = new WebServer(serverConnect.accept());
+				
+				if (verbose) {
+					System.out.println("Connecton opened. (" + new Date() + ")");
+				}
+				
+				// create dedicated thread to manage the client connection
+				Thread thread = new Thread(myServer);
+				thread.start();
+				System.out.println("Id = " + thread.getId());
+				System.out.println("Number of thread running = " + Thread.activeCount());
+			}
+			
+		} catch (IOException e) {
+			System.err.println("Server Connection error : " + e.getMessage());
+		}
+
   }
+  
+  private String getContentType(String fileRequested) {
+	  if(fileRequested.endsWith(".htm") || fileRequested.endsWith(".html"))
+		  return "text/html";
+	  else if(fileRequested.endsWith(".pdf"))
+		  return "application/pdf";
+	  else if(fileRequested.endsWith(".jpeg") || fileRequested.endsWith(".jpg"))
+		  return "image/jpeg";
+	  else if(fileRequested.endsWith(".png"))
+		  return "image/png";
+	  else
+		  return "text/plain";
+  }
+  private byte[] readFileData(File file, int fileLength) throws IOException {
+		FileInputStream fileIn = null;
+		byte[] fileData = new byte[fileLength];
+		
+		try {
+			fileIn = new FileInputStream(file);
+			fileIn.read(fileData);
+		} finally {
+			if (fileIn != null) 
+				fileIn.close();
+		}
+		
+		return fileData;
+	}
+
 }
